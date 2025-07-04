7e271d349a9a6d4242553256e04941a8c3ee3105
Parul Mishra <146674942+Parulm16@users.noreply.github.com>
2025-07-04 13:15:29 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+# utils/config.py
+
+# Your NewsAPI key
+NEWSAPI_KEY = "51450329b3cc4eb784f9b4837c8431c7"
+
+# Sectors and keywords
+SECTOR_KEYWORDS = {
+    "Finance": ["bank", "rbi", "interest", "loan", "hdfc", "sbi", "nbfc","icici","kotak","axis"],
+    "IT": ["infosys", "tcs", "wipro", "tech", "software", "digital"],
+    "Metals": ["steel", "aluminum", "mining", "metal", "jsw"],
+    "Auto": ["ev", "vehicle", "maruti", "motors", "auto","mahindra"],
+    "Pharma": ["pharma", "vaccine", "drug", "cipla", "lab","hospital","health"],
+    "FMCG": ["itc", "hul", "consumer", "fast moving", "staples", "dabur"],
+    "Oil & Gas": ["oil", "gas", "reliance", "petro", "castrol", "coal"],
+}
